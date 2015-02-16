e7af5642a338e13b21bf2520b69fc51ba1c379e4
rodrigolive <rodrigolive@gmail.com>
2015-02-15 20:48:56 -0600

    version inicial

---
+++ goodbye.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Adiós. Este fichero ha sido compilado para el entorno ${bl} en el pase ${job_name}\n");
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hola.  Este ejecutable ha sido compilado como parte del pase ${job_name}\nEsto es para la PoC con Lisette");
+    return 0;
+}
