8cee6723ed9acd911b5415e3b1edf049fe7ef959
gvanneste <geert@clarive.com>
2016-02-10 09:54:48 +0100

    second feb revision

--- goodbye.c
+++ goodbye.c
@@ -5,5 +5,6 @@ int main(void)
     printf("Goodbye. This file has been compiled for environment ${bl} in de job ${job_name}\n");
     printf("August revision. ");
     printf("February revision. ");
+    printf("February second revision. ");
     return 0;
 }T
